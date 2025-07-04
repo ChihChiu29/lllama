2445189899aef4042ca22ed1918e55f9e89e90ac
chih chiu <chih.chiu.19@gmail.com>
2025-07-04 03:31:17 -0700

    include cuda version

---
+++ exe/llamacppcuda.bat
@@ -0,0 +1,4 @@
+@echo off
+REM llamafile with pre-defined flags. At minimum, use `-m <model>` to start.
+REM Use /help to get a list of commands, use /dump to save, /upload to load, and /forget to free context window.
+.\llama.cpp.cuda\llama-cli.exe --conversation --color --no-perf --mlock --offline --no-display-prompt --log-colors --verbosity 0 -f ..\prompts\system_prompt.txt %*
